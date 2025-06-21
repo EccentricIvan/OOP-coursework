bb9ac2c0ced56659288a7334035f581953aedda9
EccentricIvan <99508766+EccentricIvan@users.noreply.github.com>
2025-06-21 14:10:58 +0300

    Add files via upload

---
+++ i_Abstraction_User (1).java
@@ -0,0 +1,19 @@
+package utms;
+
+public abstract class User {
+    private String name;
+    private String userId;
+
+    public User(String name, String userId) {
+        this.name = name;
+        this.userId = userId;
+    }
+
+    public String getName() { return name; }
+    public String getUserId() { return userId; }
+
+    public void setName(String name) { this.name = name; }
+    public void setUserId(String userId) { this.userId = userId; }
+
+    public abstract void requestTransport();
+}
